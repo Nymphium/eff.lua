c050ca4a9c12bedd44a1299bd30ef331e51a3318
Kawahara Satoru <s1311350@gmail.com>
2019-02-04 21:08:03 +0900

    add nulti-effect handler

--- src/eff.lua
+++ src/eff.lua
@@ -125,9 +125,85 @@ handler = function(eff, vh, effh)
   end
 end
 
+local function get_effh(eff, effeffhs)
+  eff = tostring(eff)
+
+  for i = 1, #effeffhs do
+    if effeffhs[i][1] == eff then
+      return effeffhs[i][2]
+    end
+  end
+end
+
+local handlers
+handlers = function(vh, effeffhs)
+  return function(th)
+    local gr = create(th)
+
+    local handle
+    local continue
+
+    local rehandles = function(arg, k)
+      return handlers(function(...) return continue(gr, ...) end, effeffhs)(function()
+        return k(arg)
+      end)
+    end
+
+    handle = function(r)
+      if not is_eff_obj(r) then
+        return vh(r)
+      end
+
+      if r.cls == Eff.cls then
+        local effh = get_effh(r.eff, effeffhs)
+        if effh then
+          return effh(function(arg)
+            return continue(gr, arg)
+          end, unpack(r.arg))
+        else
+          return Resend(r, function(arg)
+            return continue(gr, arg)
+          end)
+        end
+      elseif r.cls == Resend.cls then
+        local effh = get_effh(r.eff.eff, effeffhs)
+        if effh then
+          return effh(function(arg)
+            return rehandles(arg, r.continue)
+          end, unpack(r.arg))
+        else
+          return Resend(r, function(arg)
+            return rehandles(arg, r.continue)
+          end)
+        end
+      end
+    end
+
+    continue = function(co, arg)
+      local st, r = resume(co, arg)
+      if not st then
+        if type(r) == "string" and
+        (r:match("attempt to yield from outside a coroutine")
+         or r:match("cannot resume dead coroutine"))
+        then
+            return error("continuation cannot be performed twice")
+        else
+          return error(r)
+        end
+      else
+        return handle(r)
+      end
+    end
+
+    return continue(gr, nil)
+  end
+end
+
+
 return {
   Eff = Eff,
   perform = yield,
   handler = handler,
+  handlers = handlers
 }
 
