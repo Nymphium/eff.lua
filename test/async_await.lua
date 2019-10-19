20918750471ae7ddd5a9a7a42f2af5eae3100abd
Kawahara Satoru <s1311350@gmail.com>
2019-10-20 04:46:48 +0900

    fix test

--- test/async_await.lua
+++ test/async_await.lua
@@ -3,10 +3,64 @@
 local eff = require('src/eff')
 local inst, perform, handler = eff.inst, eff.perform, eff.handler
 
-local imut = require('spec/utils/imut')
-local ref = require('spec/utils/ref')
-local randoms = require('spec/utils/SEED')
-randoms.init()
+local imut
+do
+  table.move = table.move
+  or function(src, from, to, on, dst)
+    for  i = from, to do
+      dst[i + on - 1] = src[i]
+    end
+
+    return dst
+  end
+
+  local cp = function(t)
+    return table.move(t, 1, #t, 1, {})
+  end
+
+  local cons = function(e, t)
+    local ret = cp(t)
+    table.insert(ret, e)
+    return ret
+  end
+
+  local rev = function(t)
+    local ret = {}
+
+    for i = #t, 1, -1 do
+      table.insert(ret, t[i])
+    end
+
+    return ret
+  end
+
+  imut = {
+    cp = cp,
+    cons = cons,
+    rev = rev
+  }
+end
+
+local ref
+do
+  local newref = function()
+    return setmetatable({
+      content = nil,
+      get = function(self)
+        return self.content
+      end
+    }, {
+      __call = function(self, v)
+        self.content = v
+        return self
+      end,
+      __bnot = function(self)
+        return self.content
+      end})
+  end
+
+  ref = function(v) return newref()(v) end
+end
 
 local Waiting = function(conts)
   return { conts, cls =  "waiting" }
@@ -46,100 +100,69 @@ end
 local run = function(main)
   local function fork(pr, main)
     return handler(Eff,
-      function(v)
-        local pp = pr:get()
-        local l
+                   function(v)
+                     local pp = pr:get()
+                     local l
 
-        if pp.cls == "waiting" then
-          l = pp[1]
-        else
-          error("impossible")
-        end
+                     if pp.cls == "waiting" then
+                       l = pp[1]
+                     else
+                       error("impossible")
+                     end
 
-        for _, k in ipairs(l) do
-          enqueue(function() return k(v) end)
-        end
+                     for _, k in ipairs(l) do
+                       enqueue(function() return k(v) end)
+                     end
 
-        pr(Done(v))
-        return dequeue()
-      end,
-      function(v, k)
-        if v.type == "async" then
-          local pr_ = ref(Waiting{})
-          enqueue(function() return k(pr_) end)
-          return fork(pr_, v[1])
-        elseif v.type == "yield" then
-          enqueue(function() return k() end)
-          return dequeue()
-        elseif v.type == "await" then
-          local p = v[1]
-          local pp = p:get()
+                     pr(Done(v))
+                     return dequeue()
+                   end,
+                   function(v, k)
+                     if v.type == "async" then
+                       local pr_ = ref(Waiting{})
+                       enqueue(function() return k(pr_) end)
+                       return fork(pr_, v[1])
+                     elseif v.type == "yield" then
+                       enqueue(function() return k() end)
+                       return dequeue()
+                     elseif v.type == "await" then
+                       local p = v[1]
+                       local pp = p:get()
 
-          if pp.cls == "done" then
-            return k(pp[1])
-          elseif pp.cls == "waiting" then
-            p(Waiting(imut.cons(k, pp[1])))
-            return dequeue()
-          end
-        end
-      end)(main)
+                       if pp.cls == "done" then
+                         return k(pp[1])
+                       elseif pp.cls == "waiting" then
+                         p(Waiting(imut.cons(k, pp[1])))
+                         return dequeue()
+                       end
+                     end
+                   end)(main)
   end
 
   return fork(ref(Waiting{}), main)
 end
 
-insulate("async await test", function()
-  randomize(false)
-
-  spy.on(_G, "print")
-
-  local main = function()
-    local task = function(name)
-      return function()
-        print(("Starting %s"):format(name))
-        local v = math.random(100)
-        print(("Yielding %s"):format(name))
-        yield()
-        print(("Ending %s with %d"):format(name, v))
-        return v
-      end
+local main = function()
+  local task = function(name)
+    return function()
+      print(("Starting %s"):format(name))
+      local v = math.random(100)
+      print(("Yielding %s"):format(name))
+      yield()
+      print(("Ending %s with %d"):format(name, v))
+      return v
     end
-
-    local pa = async(task "a")
-
-    local pb = async(task "b")
-    local pc = async(function()
-      return await(pa) + await(pb)
-    end)
-
-    print(("sum is %d"):format(await(pc)))
-    assert(await(pa) + await(pb) == await(pc))
   end
 
-  describe("run", function()
-    run(main)
+  local pa = async(task "a")
 
-    it("check first resuming a", function()
-      assert.spy(print).was_called_with("Starting a")
-      assert.spy(print).was_called_with("Yielding a")
-    end)
-
-    it("check first resuming b", function()
-      assert.spy(print).was_called_with("Starting b")
-      assert.spy(print).was_called_with("Yielding b")
-    end)
-
-    it("check task return value", function()
-      randoms.init()
-
-      local v1 = math.random(100)
-      local v2 = math.random(100)
-
-      assert.spy(print).was_called_with(("Ending %s with %d"):format("a", v1))
-      assert.spy(print).was_called_with(("Ending %s with %d"):format("b", v2))
-      assert.spy(print).was_called_with(("sum is %d"):format(v1 + v2))
-      assert.spy(print).was_called(7)
-    end)
+  local pb = async(task "b")
+  local pc = async(function()
+    return await(pa) + await(pb)
   end)
-end)
 
+  print(("sum is %d"):format(await(pc)))
+  assert(await(pa) + await(pb) == await(pc))
+end
+
+run(main)
