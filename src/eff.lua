feece9959f0b0e00dc6ee29d19edbc3ff4971cda
Kawahara Satoru <s1311350@gmail.com>
2019-10-21 06:43:24 +0900

    add shallow handler

--- src/eff.lua
+++ src/eff.lua
@@ -76,9 +76,70 @@ local function handler(h)
   end
 end
 
+local function shallow_handler(h)
+  return function(th)
+    local co = create(th)
+
+    local handle
+    local continue = function(arg)
+      return handle(resume(co, arg))
+    end
+
+    local rehandle = function(k)
+      return function(arg)
+        local newh = {
+          val = continue,
+        }
+
+        for op, effh in pairs(h) do
+          newh[op] = effh
+        end
+
+        return shallow_handler(newh)(function()
+          return k(arg)
+        end)
+      end
+    end
+
+    local continue_ = function(co_)
+      return function(arg)
+        local r = resume(co_, arg)
+
+        if not is_eff_obj(r) then
+          return r
+        else
+          return resend(r.eff, r.arg, function(arg) return resume(co, arg) end)
+        end
+      end
+    end
+
+    handle = function(r)
+      if not is_eff_obj(r) then return h.val(r)
+      else
+        local effh = h[r.eff]
+
+        if     r.type == performT and effh then
+          return effh(r.arg, continue_(co))
+        elseif r.type == performT          then
+          return resend(r.eff, r.arg, continue)
+        elseif r.type == resendT  and effh then
+          return effh(r.arg, continue_(create(r.continue)))
+        elseif r.type == resendT           then
+          return resend(r.eff, r.arg, rehandle(r.continue))
+        else
+          return error("unreachable")
+        end
+      end
+    end
+
+    return continue(nil)
+  end
+end
+
 return {
   inst = inst,
   perform = perform,
   handler = handler,
+  shallow_handler = shallow_handler,
 }
 
